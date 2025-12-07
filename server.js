8862f50478aa6ca80c0a2f72d5d95de288c45574
gaurav01singh <gauravsingh02195@gmail.com>
2025-12-07 13:30:08 +0530

    vercel server fix

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
-const serverless = require('@vendia/serverless-express');
-const app = require('../src/app');
+const app = require('./src/app');
+require('dotenv').config();
 
-module.exports = serverless({ app });
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+export default app;
